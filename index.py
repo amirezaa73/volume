e03e8335a55e73945b7012b4d6c6a4526e9570dd
amirezaa73 <109097171+amirezaa73@users.noreply.github.com>
2025-01-26 12:30:35 +0330

    Update index.py

--- index.py
+++ index.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "API is running on Vercel!"})
 
-@app.route('/binance', methods=['GET'])
-def get_binance_price():
-    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+@app.route('/bybit', methods=['GET'])
+def get_bybit_data():
+    url = "https://api.bybit.com/v5/market/kline?category=spot&symbol=BTCUSDT&interval=60"
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
-        return jsonify(response.json())  # Return the JSON response from Binance
+        return jsonify(response.json())  # Return the JSON response from Bybit
     except requests.exceptions.RequestException as e:
-        # Return the exact error response from Binance, if available
+        # Return the exact error response from Bybit, if available
         if response is not None and response.text:
-            return jsonify({"error": "Binance API Error", "details": response.text}), response.status_code
+            return jsonify({"error": "Bybit API Error", "details": response.text}), response.status_code
         else:
             return jsonify({"error": "Request failed", "details": str(e)}), 500
